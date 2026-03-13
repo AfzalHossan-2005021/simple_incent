6a566225c97ab9fcf7c57d05ed6d24c542c30721
Afzal Hossan <afzalhossan2005021@gmail.com>
2026-03-13 17:49:53 +0600

    init fixed.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,1 @@
-from .smart_align import smart_pairwise_align as pairwise_align
-from .INCENT import neighborhood_distribution, cosine_distance
+from .INCENT import pairwise_align, neighborhood_distribution, cosine_distance
